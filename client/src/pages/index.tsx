620f0d0941040f67e80c42836d12911027389218
Brijesh <ops@brijesh.dev>
2024-06-28 02:32:38 +0530

    feat(client): create client and basic UI for managing projects
    Created a web dashboard for watchman using Next.js, I could have used go html templates but using Next.js I can finish building frontend faster
    
    Might build a client using server side go templates if this works well enough
    
    Also created pages for managing projects

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import MainLayout from "@/components/MainLayout";
+
+const Homepage = () => {
+  return (
+    <MainLayout>
+      <h1 className="text-xl font-medium">Homepage</h1>
+      <p className="my-2">
+        Please check this page later, as it is still under construction.
+      </p>
+    </MainLayout>
+  );
+};
+
+export default Homepage;
---
+++ client/src/pages/projects/index.tsx
@@ -0,0 +1,43 @@
+import { ListProjects } from "@/api/projects";
+import { PrimaryButton } from "@/components/Button";
+import MainLayout from "@/components/MainLayout";
+import Table from "@/components/table";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const ProjectsHomePage = () => {
+  const router = useRouter();
+
+  const [projects, setProjects] = useState<any>([]);
+
+  useEffect(() => {
+    ListProjects(setProjects);
+  }, []);
+
+  return (
+    <MainLayout>
+      <div className="mb-4 flex justify-between items-center">
+        <h1 className="text-xl font-medium">Projects</h1>
+        <PrimaryButton
+          text="Create Project"
+          onClick={() => router.push("/projects/create")}
+        />
+      </div>
+      {projects.data !== undefined && (
+        <Table
+          columns={["Name", "ID"]}
+          data={projects.data.map((project: any) => {
+            return {
+              name: (
+                <Link href={"/projects/" + project.id}>{project.name}</Link>
+              ),
+              id: project.id,
+            };
+          })}
+        />
+      )}
+    </MainLayout>
+  );
+};
+export default ProjectsHomePage;
