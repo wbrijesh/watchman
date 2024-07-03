242871c6f0dd3c1670d49334a7f551c68b7a518e
Brijesh <ops@brijesh.dev>
2024-07-03 19:38:37 +0530

    feat(chart): use tremor chart for showing log frequency
    Tried using D3 and while I might have been able to make charts exactly the way it would have taken too much time, so chose to go with a simple looking charts library

--- client/src/pages/logs/index.tsx
+++ client/src/pages/logs/index.tsx
@@ -1,10 +1,8 @@
 import MainLayout from "@/components/MainLayout";
-import BarChartExample from "@/components/visualisation/Bar-Chart";
-import { BarChart } from "@tremor/react";
 import Head from "next/head";
 import Link from "next/link";
 
-const Homepage = () => {
+const LogsIndexPage = () => {
   return (
     <MainLayout>
       <Head>
@@ -14,13 +12,12 @@ const Homepage = () => {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>"
         />
       </Head>
-      <h1 className="text-xl font-medium">Homepage</h1>
+      <h1 className="text-xl font-medium">Logs Index Page</h1>
       <p className="my-2">
         Please check this page later, as it is still under construction.
       </p>
-      <BarChartExample />
     </MainLayout>
   );
 };
 
-export default Homepage;
+export default LogsIndexPage;
